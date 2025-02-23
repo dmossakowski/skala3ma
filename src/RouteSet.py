34ff35dcd5230398f0b8f2fed0b2b4cb31ff9ec7
David Mossakowski <dmossakowski@gmail.com>
2025-02-23 23:09:13 +0100

    missing files

---
+++ src/RouteSet.py
@@ -0,0 +1,43 @@
+import uuid
+
+class RouteSet:
+    def __init__(self):
+        self.routes = {}
+        self.routes['id'] = str(uuid.uuid4().hex)
+        self.routes['name'] = "Default"
+
+    def generate_dummy_routes(self, num_routes):
+        routesA = []
+        for i in range(1, num_routes + 1):
+            route = {
+                'id': str(uuid.uuid4().hex),
+                'routenum': str(i),
+                'line': str(i),
+                'colorfr': 'Vert',
+                'color1': '#2E8B57',
+                'color2': '',
+                'grade': '4b',
+                'name': 'Dummy route '+str(i),
+                'openedby': '',
+                'opendate': '',
+                'notes': 'dummy routes'
+            }
+            routesA.append(route)
+        self.routes['routes'] = routesA
+        
+        return self.routes
+
+
+    def get_routes(self):
+        return self.routes
+
+    def get_id(self):
+        return self.routes['id']
+
+# Example usage
+if __name__ == "__main__":
+    route_set = RouteSet()
+    route_set.generate_dummy_routes(5)
+    
+    for route in route_set.get_routes():
+        print(route)
